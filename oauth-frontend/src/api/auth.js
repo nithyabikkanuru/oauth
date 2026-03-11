96539e5d4fc5a73b8fe60fba066d6d14e55b012c
Nithya bikkanuru <nithyabikkanuru@gmail.com>
2026-03-11 21:02:54 +0530

    Update auth.js

--- oauth-frontend/src/api/auth.js
+++ oauth-frontend/src/api/auth.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 export const api = axios.create({
-  baseURL: "http://localhost:5000",
+  // baseURL: "http://localhost:5000",
+  const API = "https://oauth-backend.onrender.com/api",
   withCredentials: true
 });
